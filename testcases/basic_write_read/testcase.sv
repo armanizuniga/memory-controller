d74d29918e4fba981d1d8b8a1e15e02216cc0f7a
Armani Zuniga <armanizuniga@Armanis-MacBook-Pro-2.local>
2024-09-23 20:37:12 -0500

    Added End of Simulation Checks and also created a task to monitor bus activity. When bus acitivty is idle for 100 clock cycles, task invokes

--- testcases/basic_write_read/testcase.sv
+++ testcases/basic_write_read/testcase.sv
@@ -10,7 +10,36 @@ program testcase(//Inputs to Design, Outputs from Program <-- Signals are to be 
                  //Output from Design, Input to Program <-- Signals are to be monitored
 				 input logic ready_sys
                 );
+  
+   // Keep count of READ and WRITE opertaions for final block
+   int write_count = 0;
+   int read_count = 0;
+   bit test_passed = 1; // Assuming the test passes unless an error is detected
+  
+   // Variables to monitor bus activities
+   int idle_cycles = 0;
+  
+   // Monitor bus activity for ending Simulation
+   task automatic check_bus_activity;
+     forever begin
+       @(posedge clk);
+        if (cmd_valid_sys || ready_sys || we_sys || (data_sys !== 8'bz)) begin
+                idle_cycles = 0;
+            end else begin
+                idle_cycles++;
+                if (idle_cycles % 10 == 0) // Print every 10 idle cycles
+                    $display("Bus idle for %0d cycles", idle_cycles);
+            end
 
+            if (idle_cycles >= 100) begin
+                $display("Bus idle for 100 clock cycles. Ending simulation.");
+                $finish;
+                break;
+            end
+        end
+    endtask
+     
+  
    initial begin
      reset = 			1;
      we_sys = 			0;
@@ -19,12 +48,18 @@ program testcase(//Inputs to Design, Outputs from Program <-- Signals are to be 
      data_sys = 		8'bz;
      #100 reset = 		0;
      
+     // Start the bus activity monitoring
+     fork
+        check_bus_activity();
+     join_none
+     
      for (int i = 0; i < 4; i++) begin
        @(posedge clk);
        addr_sys = 		i;
        data_sys = 		$urandom_range(0,255);
        cmd_valid_sys = 	1;
        we_sys = 		1;
+       write_count++;
        
        @(posedge ready_sys);
        $display("%5dns: Writing: address=%0d, write data=8'h%2h", $time, i, data_sys);
@@ -44,6 +79,7 @@ program testcase(//Inputs to Design, Outputs from Program <-- Signals are to be 
        addr_sys = i;
        cmd_valid_sys = 1;
        we_sys = 0;
+       read_count++;
        
        @(posedge ready_sys);
        @(posedge clk);
@@ -53,7 +89,16 @@ program testcase(//Inputs to Design, Outputs from Program <-- Signals are to be 
        cmd_valid_sys = 0;
      end
      
-     #10 $finish;
+     repeat(150) @(posedge clk);
+     
    end
+  
+   final begin
+        $display("\n***** End Of Simulation Summary *****");
+        $display("Number of WRITE operations performed: %0d", write_count);
+        $display("Number of READ operations performed: %0d", read_count);
+        $display("Test status: %s", test_passed ? "PASS" : "FAIL");
+        $display("*************************************\n");
+    end
 
 endprogram
